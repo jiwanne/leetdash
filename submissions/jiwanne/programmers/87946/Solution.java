df313edca91205b5873d3b5f058bc1ec162a61ef
jiwanne <juckyjoo225@naver.com>
2026-08-24 11:46:37 +0900

    solve: programmers 87946

--- submissions/jiwanne/programmers/87946/Solution.java
+++ submissions/jiwanne/programmers/87946/Solution.java
@@ -1,23 +1,27 @@
 class Solution {
     
-    static int answer;
-    static boolean [] v;    
+    public int answer;
+    public boolean [] v;    
     
     
-    
+    void dfs(int x, int k , int[][] d) {
+        
+        for(int i = 0; i < d.length; i++) {
+            if(!v[i] && k >= d[i][0]) {
+                v[i] = true;
+                dfs(x + 1 , k - d[i][1] , d);
+                v[i] = false;
+            }
+        }
+        answer = Math.max(x , answer);        
+    }
     
     
     public int solution(int k, int[][] dungeons) {
-        int n = dungenons.length;
+        int n = dungeons.length;
         v = new boolean[n];
         
-        dfs(0, v , dungenons);
-        
-        
-        
-        
-        
-        
+        dfs(0, k , dungeons);
             
         return answer;
     }
